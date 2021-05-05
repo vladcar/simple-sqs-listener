2cc4ce423ffa2808fbdaa3f1fa61f95f3ce7e5bc
vladc <logdoompwnz@gmail.com>
2021-05-05 09:48:44 +0300

    Revert "initial"
    This reverts commit 87aa6c40

--- core/src/main/java/com/vladc/sqslistener/ErrorHandler.java
+++
@@ -1,8 +0,0 @@
-package com.vladc.sqslistener;
-
-import software.amazon.awssdk.services.sqs.model.Message;
-
-public interface ErrorHandler {
-
-  void onError(Message message, Exception exception);
-}
--- core/src/main/java/com/vladc/sqslistener/MessageHandler.java
+++
@@ -1,8 +0,0 @@
-package com.vladc.sqslistener;
-
-import software.amazon.awssdk.services.sqs.model.Message;
-
-public interface MessageHandler {
-
-  void handle(Message message);
-}
